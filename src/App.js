2eb1fe57cd76a061508f1aa0ebb138ba7c505786
Pavel <pawelv9@gmail.com>
2024-05-09 16:17:59 +0300

    added AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 
 function App() {
-  const items = [
-    {
-      id: '1',
-      label: 'Can I use React on a project',
-      content: 'You can use React on any project you want'
-    },
-    {
-      id: '2',
-      label: 'Can I use React on a project',
-      content: 'You can use React on any project you want'
-    },
-    {
-      id: '3',
-      label: 'Can I use React on a project',
-      content: 'You can use React on any project you want'
-    },
-  ]
+  
 
-  return (
-     <Accordion items = { items } />
-  )
+return <Dropdown />
 }
 
 export default App;
