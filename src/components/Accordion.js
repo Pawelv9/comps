271ea643814e6c09c57a8d30b0d4460bae85cada
Pavel <pawelv9@gmail.com>
2024-05-09 13:25:30 +0300

    added styles

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -12,24 +12,24 @@ function Accordion({ items }) {
     const isExpanded = index === expandedIndex; 
 
     const icon = (
-    <span>
+    <span className='text-2xl'>
       {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
     </span>
     )
 
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>
-        {icon}
+        <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' onClick={() => handleClick(index)}>
         {item.label}
+        {icon}
         </div>
-        <div>{isExpanded && <div>{item.content}</div>}</div>
+        <div>{isExpanded && <div className='border-b p-5'>{item.content}</div>}</div>
       </div>
     )
   })
 
   return (
-    <div>{renderedItems}</div>
+    <div className='border-x border-t rounded'>{renderedItems}</div>
   )
 }
 
