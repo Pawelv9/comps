e36a24fbf2090baf611b37a8f77db303ff50f18f
Pawelv9 <Pawelv9@gmail.com>
2024-05-27 15:57:41 +0300

    refactoring and styles

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
+import { GoChevronDown } from 'react-icons/go'
 
-const Dropdown = ({ options, selection, onSelect }) => {
+const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -9,12 +10,12 @@ const Dropdown = ({ options, selection, onSelect }) => {
 
   const handleOptionClick = (option) => {
     setIsOpen(false)
-    onSelect(option);
+    onChange(option);
   }
 
   const renderOptions = options.map(option => {
     return(
-      <div
+      <div className='hover:bg-sky-100 rounded cursor-pointer p-1'
           onClick={() => handleOptionClick(option)}
           key={option.value}>
         {option.label}
@@ -22,12 +23,18 @@ const Dropdown = ({ options, selection, onSelect }) => {
     )
   })
 
-
-
   return (
-    <div>
-      <div onClick = {handleClick}>{selection?.label || 'Select..'}</div>
-      {isOpen && <div>{renderOptions}</div>}
+    <div className='w-48 relative'>
+      <div className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full'
+           onClick = {handleClick}>
+        {value?.label || 'Select..'}
+        <GoChevronDown />
+      </div>
+      {isOpen && (
+          <div className='absolute top-full border p-3 shadow bg-white w-full'>
+            {renderOptions}
+          </div>
+      )}
     </div>
   )
 }
