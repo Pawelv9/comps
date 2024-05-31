71c05209a92cedd266117ea51e27c9d223aefbbe
Pawelv9 <Pawelv9@gmail.com>
2024-05-31 21:36:13 +0300

    added programmatic navigation

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,14 +1,29 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 
 const NavigationContext = createContext();
 
 function NavigationProvider({children}) {
     const [currentPath, setCurringPath] = useState(window.location.pathname);
 
-    return <NavigationContext.Provider value={{}}>
-        {currentPath}
-        {children}
-    </NavigationContext.Provider>
+    useEffect(() => {
+        const handler = () => {
+            setCurringPath(window.location.pathname)
+        }
+        window.addEventListener('popstate', handler);
+
+        return () => {
+            window.removeEventListener('popstate', handler);
+        }
+    }, []);
+
+    const navigate = (to) => {
+        window.history.pushState({}, '', to)
+        setCurringPath(to)
+    }
+
+    return <NavigationContext.Provider value={{ currentPath, navigate }}>{children}
+    {children}
+</NavigationContext.Provider>
 }
 
 export { NavigationProvider };
