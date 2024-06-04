cb2992308c1560aa00d6cf8de71abccd564c2079
Pawelv9 <Pawelv9@gmail.com>
2024-06-04 21:56:07 +0300

    added styles to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,12 @@
 
 function Modal(props) {
     return (
-        <div>Modal</div>
+        <div>
+            <div className='absolute inset-0 bg-gray-300 opacity-80'></div>
+            <div className='absolute inset-40 p-10 bg-white'>
+                Modal!
+            </div>
+        </div>
     );
 }
 
