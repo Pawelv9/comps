a712a8922be5b0556cdecb888557a111aae25ab5
Pawelv9 <Pawelv9@gmail.com>
2024-06-07 17:12:34 +0300

    building the rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
-function Table( data ) {
+function Table({ data }) {
+    const renderedRows = data.map(fruit => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+
     return (
-        <div>{data.length}</div>
+        <table>
+            <thead>
+            <tr>
+                <th>Fruit</th>
+                <th>Color</th>
+                <th>Score</th>
+            </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
     );
 }
 
