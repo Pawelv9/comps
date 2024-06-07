2509dc4b31aef142efca1ef3dc15b4ac4fd9b21f
Pawelv9 <Pawelv9@gmail.com>
2024-06-07 16:59:27 +0300

    communicating data to the table

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -2,9 +2,16 @@ import Table from "../components/Table";
 
 
 function TablePage() {
+    const data = [
+        {name: 'Orange', color: 'bg-orange-500', score: 5},
+        {name: 'Apple', color: 'bg-red-500', score: 3},
+        {name: 'Orange', color: 'bg-yellow-500', score: 1},
+        {name: 'Lime', color: 'bg-green-500', score: 4},
+    ]
+
     return (
         <div>
-            <Table />
+            <Table data={data} />
         </div>
     );
 }
