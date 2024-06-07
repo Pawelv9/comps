345d570c034f215889ea8d82bb9b8b88b4dd8d84
Pawelv9 <Pawelv9@gmail.com>
2024-06-07 21:09:23 +0300

    adding a Key Function

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,18 @@
 import React from 'react';
 
-function Table({ data, config }) {
+function Table({ data, config, keyFn }) {
     const renderedHeaders = config.map(( column ) => {
         return <th key={column.label}>{column.label}</th>
     })
 
-    const renderedRows = data.map(fruit => {
+    const renderedRows = data.map(rowData => {
         const renderedCells = config.map(column => {
-            return <td className='p-3' key={column.label}>{column.render(fruit)}</td>
+            return <td className='p-3' key={column.label}>{column.render(rowData)}</td>
         })
 
         return (
-            <tr className='border-b' key={fruit.name}>
+            <tr className='border-b' key={keyFn(rowData)}>
                 {renderedCells}
-                {/*<td className='p-3'>{config[0].render(fruit)}</td>*/}
-                {/*<td className='p-3'>*/}
-                {/*    {config[1].render(fruit)}*/}
-                {/*    /!*<div className={`p- 3 m-2 ${config[1].render(fruit)}`}></div>*!/*/}
-                {/*</td>*/}
-                {/*<td className='p-3'>{config[2].render(fruit)}</td>*/}
             </tr>
         );
     });
