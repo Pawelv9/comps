220b64ad81bee7ffa8e18d9776ab3169df8824b5
Pawelv9 <Pawelv9@gmail.com>
2024-06-10 22:04:31 +0300

    added counterPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ButtonPage from "./pages/ButtonPage";
 import Sidebar from './components/Sidebar';
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
 
@@ -26,6 +27,9 @@ return (
         </Route>
         <Route path='/table'>
             <TablePage />
+        </Route>
+        <Route path='/counter'>
+            <CounterPage initialCount={10} />
         </Route>
     </div>
   </div>
