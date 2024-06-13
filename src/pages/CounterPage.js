9d06b53d0ae1a10f65c9ee1ee9c91975928e4e12
Pawelv9 <Pawelv9@gmail.com>
2024-06-13 22:50:55 +0300

    updated counterPage

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,35 +1,47 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
-function CounterPage({initialCount}) {
-    const [count, setCount] = useState(initialCount);
-    const [valueToAdd, setValueToAdd] = useState(0)
+const reducer = (state, action) => {
+    return {
+        ...state,
+        count: state.count + 1,
+    }
+}
+
+function CounterPage({ initialCount }) {
+    // const [count, setCount] = useState(initialCount);
+    // const [valueToAdd, setValueToAdd] = useState(0)
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0,
+    })
 
     const increment = () => {
-        setCount(count + 1)
+        dispatch();
     }
 
     const decrement = () => {
-        setCount(count - 1)
+        // setCount(count - 1)
     }
 
     const handleChange = (event) => {
         const value  = parseInt(event.target.value) || 0;
 
-        setValueToAdd(value)
+        // setValueToAdd(value)
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        setCount(count + valueToAdd);
-        setValueToAdd(0)
+        // setCount(count + valueToAdd);
+        // setValueToAdd(0)
     }
 
     return (
         <Panel>
-            <h1 className='text-lg'>Count is {count}</h1>
+            <h1 className='text-lg'>Count is {state.count}</h1>
             <div className='flex flex-row'>
                 <Button onClick={increment}>
                     Increment
@@ -42,7 +54,7 @@ function CounterPage({initialCount}) {
             <form onSubmit={handleSubmit}>
                 <label>Add a lot</label>
                 <input
-                    value={valueToAdd || ''}
+                    value={state.valueToAdd || ''}
                     onChange={handleChange}
                     type='number'
                     className='p-1 m-3 bg-gray-50 border border-gray-300'
